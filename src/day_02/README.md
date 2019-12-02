6a92b71a05958d0149e24ab95779d14df6fb8022
Matt Campbell <matt@mattcampbell.net>
2019-12-02 00:53:40 -0500

    Day 02

---
+++ src/day_02/README.md
@@ -0,0 +1,5 @@
+# Day 2: 1202 Program Alarm
+
+See https://adventofcode.com/2019/day/2
+
+Relevant snippets are included in source code comments
---
+++ src/day_02/part_02.py
@@ -0,0 +1,33 @@
+import sys
+from .part_01 import parse_input, intcode_program
+
+
+def find_noun_and_verb(base_state, needle):
+    for noun in range(0, 100):
+        for verb in range(0, 100):
+            state = base_state[:]  # Copy so base_state is not mutated
+            state[1] = noun
+            state[2] = verb
+            intcode_program(state)
+
+            if state[0] == needle:
+                return noun, verb
+
+    return None, None
+
+
+def main(input_file):
+    base_state = parse_input(input_file)
+    needle = 19690720
+
+    noun, verb = find_noun_and_verb(base_state, needle)
+
+    if None in (noun, verb):
+        print('Could not find noun/verb that would produce {}'.format(needle))
+        return
+
+    print(100 * noun + verb)
+
+
+if __name__ == '__main__':
+    main(sys.stdin)
