85bd1596ff463359c78c59853cd557f77de58e0b
Matt Campbell <matt@mattcampbell.net>
2019-12-01 19:09:48 -0500

    Add per-day runner / input manager thing

---
+++ main.py
@@ -0,0 +1,59 @@
+import subprocess
+import argparse
+import shutil
+import requests
+from os import path, makedirs
+from secrets import AOC_SESSION_ID
+
+
+def download_input(day, file_name, session_id=AOC_SESSION_ID):
+    url = 'https://adventofcode.com/2019/day/{}/input'.format(day)
+    cookies = {'session': AOC_SESSION_ID}
+    with requests.get(url, cookies=cookies, stream=True) as r:
+        r.raise_for_status()
+        r.raw.decode_content = True  # Content is gzip-encoded
+        with open(file_name, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+
+def get_full_path(p):
+    return path.join(path.realpath(path.dirname(__file__)), p)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--day', type=int, required=True)
+    parser.add_argument('-p', '--part', type=int, action='append')
+    args = parser.parse_args()
+
+    day = args.day
+    default_parts = [1, 2]
+    parts = args.part or default_parts
+    parts = list({p for p in parts if p in default_parts}) or default_parts
+    parts.sort()
+
+    src_dir = get_full_path('src')
+    input_dir = get_full_path('input')
+    makedirs(input_dir, exist_ok=True)
+
+    input_file = path.join(input_dir, 'day-{:02d}.txt'.format(day))
+    if not path.isfile(input_file):
+        print('{} not cached. Downloading...'.format(input_file))
+        download_input(day, input_file)
+    else:
+        print('Using cached input: {}'.format(input_file))
+
+    print('')
+    print('Day {:02d}'.format(day))
+    print('===')
+
+    for part in parts:
+        print('')
+        print('Part {:02d}:'.format(part))
+        with open(input_file) as f:
+            script = 'day-{:02d}/part_{:02d}.py'.format(day, part)
+            subprocess.run(['python', path.join(src_dir, script)], stdin=f)
+
+
+if __name__ == '__main__':
+    main()
---
+++ secrets.example.py
@@ -0,0 +1,5 @@
+# Rename this file to secrets.py
+
+# Session cookie used for downloading input data from Advent of Code
+# Find this in your browser's network inspector
+AOC_SESSION_ID = '0123456789abcdef0123456789abcdef0123456789abcdef0123456789ab'
