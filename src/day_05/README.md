15835c1a4a5e52967971b76320a4876a39b3bf98
Matt Campbell <matt@mattcampbell.net>
2019-12-05 03:06:59 -0500

    Day 05 / Part 02

--- src/day_05/README.md
+++ src/day_05/README.md
@@ -1,4 +1,4 @@
-# Day 5: ???
+# Day 5: Sunny with a Chance of Asteroids
 
 See https://adventofcode.com/2019/day/5
 
--- src/day_05/part_02.py
+++ src/day_05/part_02.py
@@ -1,9 +1,20 @@
 import sys
+from .part_01 import parse_input
+from ..intcode import vm
 
 
 def main(input_file):
-    for line in input_file:
-        print(line)
+    """
+    This time, when the TEST diagnostic program runs its input instruction to
+    get the ID of the system to test, provide it 5, the ID for the ship's
+    thermal radiator controller. This diagnostic test suite only outputs one
+    number, the diagnostic code.
+
+    What is the diagnostic code for system ID 5?
+    """
+
+    state = parse_input(input_file)
+    print(vm.run_program(state, 5))
 
 
 if __name__ == '__main__':
