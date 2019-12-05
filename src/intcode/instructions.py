0b7574582c50a878d22953e89cac0bd6be1c972b
Matt Campbell <matt@mattcampbell.net>
2019-12-05 01:08:54 -0500

    Intcode VM: Add parameter modes + program input

--- src/intcode/instructions.py
+++ src/intcode/instructions.py
@@ -6,14 +6,52 @@ class OpCode(IntEnum):
     MULTIPLY = 2
     HALT = 99
 
+    @staticmethod
+    def from_instruction_int(n):
+        """
+        Returns raw opcode and parameter mode ints from instruction int
+
+        >>> OpCode.from_instruction_int(1012)
+        (12, [0, 1])
+
+        >>> OpCode.from_instruction_int(1101012)
+        (12, [0, 1, 0, 1, 1])
+
+        >>> OpCode.from_instruction_int(99)
+        (99, [])
+
+        >>> OpCode.from_instruction_int(103)
+        (3, [1])
+        """
+
+        s = str(n)
+        opcode = int(s[-2:])
+        modes = [int(x) for x in s[:-2]]
+
+        # Modes are actually right-to-left
+        modes.reverse()
+
+        return (opcode, modes)
+
+
+class ParameterMode(IntEnum):
+    POSITION = 0
+    IMMEDIATE = 1
+
 
 class Instruction:
-    def __init__(self):
+    def __init__(self, param_modes):
         self.num_parameters = 0
+        self.param_modes = param_modes
 
     @staticmethod
-    def from_opcode(opcode):
-        return _instructions.get(opcode, None)
+    def from_instruction_int(instruction_int):
+        opcode, param_modes = OpCode.from_instruction_int(instruction_int)
+        instruction = _instructions.get(opcode, None)
+        if not instruction:
+            return None
+
+        return instruction(param_modes)
 
     def validate(self, stack, state):
         n = self.num_parameters
@@ -26,35 +64,35 @@ class Instruction:
 
 
 class InstructionAdd(Instruction):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, param_modes):
+        super().__init__(param_modes)
         self.num_parameters = 3
 
-    def exec(self, stack, state):
+    def exec(self, stack, state, program):
         super().validate(stack, state)
         state[stack[2]] = state[stack[0]] + state[stack[1]]
         return True
 
 
 class InstructionMultiply(Instruction):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, param_modes):
+        super().__init__(param_modes)
         self.num_parameters = 3
 
-    def exec(self, stack, state):
+    def exec(self, stack, state, program):
         super().validate(stack, state)
         state[stack[2]] = state[stack[0]] * state[stack[1]]
         return True
 
 
 class InstructionHalt(Instruction):
-    def exec(self, stack, state):
+    def exec(self, stack, state, program):
         super().validate(stack, state)
         return False
 
 
 _instructions = {
-    OpCode.ADD: InstructionAdd(),
-    OpCode.MULTIPLY: InstructionMultiply(),
-    OpCode.HALT: InstructionHalt(),
+    OpCode.ADD: InstructionAdd,
+    OpCode.MULTIPLY: InstructionMultiply,
+    OpCode.HALT: InstructionHalt,
 }
--- src/intcode/vm.py
+++ src/intcode/vm.py
@@ -1,23 +1,30 @@
 from .instructions import Instruction
 
 
-def run_program(state):
+def run_program(state, input=None):
     pointer = 0
     state_length = len(state)
 
+    program = {
+        'input': input,
+        'output': None
+    }
+
     while pointer < state_length:
-        opcode = state[pointer]
-        instruction = Instruction.from_opcode(opcode)
+        instruction_int = state[pointer]
+        instruction = Instruction.from_instruction_int(instruction_int)
         if not instruction:
-            raise ValueError('Unknown opcode: {}'.format(opcode))
+            raise ValueError('Unknown instruction type: {}'
+                             .format(instruction_int))
 
         stack_size = instruction.num_parameters
         pointer += 1  # Move pointer to beginning of arguments
+        stack = state[pointer:pointer + stack_size]
 
         # Instructions can return False to halt execution
-        if not instruction.exec(state[pointer:pointer + stack_size], state):
+        if not instruction.exec(stack, state, input):
             break
 
         pointer += stack_size
 
-    return state
+    return program['output']
